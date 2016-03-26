066d5452decbd488a37e626833fe0ad5811d6f00
Parker <pHabelson@gmail.com>
2016-03-26 11:25:08 -0400

    First commit:

---
+++ app/src/main/java/com/example/phwilf/p3_take2/CatalogueActivityFragment.java
@@ -0,0 +1,63 @@
+package com.example.phwilf.p3_take2;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class CatalogueActivityFragment extends Fragment {
+
+    public CatalogueActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_catalogue, container, false);
+
+        //create dummy data
+        String[] dummyData = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7",
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+        List<String> dummyForecast = new ArrayList<String>(Arrays.asList(dummyData));
+
+        //create the adapter to bind data
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_catalogue,
+                R.id.list_item_catalogue_textview,
+                dummyForecast
+        );
+
+        //bind adapter to listview
+        ListView listview = (ListView) rootView.findViewById(R.id.listview_catalogue);
+        listview.setAdapter(adapter);
+
+        return rootView;
+    }
+}
